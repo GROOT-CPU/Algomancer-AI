c3aa7a7e6113d7340642f9a7afe1adcc77a8f4fa
SRIRAM JG <2k23it53@kiot.ac.in>
2026-03-13 10:08:35 +0530

    updated frontend api connection

--- Fontend/script.js
+++ Fontend/script.js
@@ -1,22 +1,56 @@
 <script>
-async function optimizeCode() {
 
-    const code = document.getElementById("codeInput").value;
-    const language = document.getElementById("language").value;
+async function optimizeCode(){
 
-    const response = await fetch("http://127.0.0.1:8000/optimize", {
-        method: "POST",
-        headers: {
-            "Content-Type": "application/json"
-        },
-        body: JSON.stringify({
-            code: code,
-            language: language
-        })
-    });
+const code = document.getElementById("codeInput").value;
+const inputLanguage = document.getElementById("inputLanguage").value;
+const outputLanguage = document.getElementById("outputLanguage").value;
 
-    const data = await response.json();
-
-    document.getElementById("result").innerText = data.optimized_code;
+if(code.trim() === ""){
+alert("Please enter code");
+return;
 }
+
+document.getElementById("outputCode").innerText = "Algomancer is processing your code...";
+
+try{
+
+const response = await fetch("https://algomancer.onrender.com/optimize",{
+
+method:"POST",
+
+headers:{
+"Content-Type":"application/json"
+},
+
+body: JSON.stringify({
+code: code,
+input_language: inputLanguage,
+output_language: outputLanguage
+})
+
+});
+
+const data = await response.json();
+
+document.getElementById("outputCode").innerText = data.optimized_code;
+
+}catch(error){
+
+document.getElementById("outputCode").innerText = "Error connecting to backend";
+
+}
+
+}
+
+function copyCode(){
+
+const code = document.getElementById("outputCode").innerText;
+
+navigator.clipboard.writeText(code);
+
+alert("Code copied!");
+
+}
+
 </script>
